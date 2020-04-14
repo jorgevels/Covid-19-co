c520dffed1a71ede36b99ecd4bc90a0f49c09b1d
jorgevels <jorgevelasquez006@hotmail.com>
2020-04-14 15:25:54 -0500

    Ruta del home

--- src/routes/App.js
+++ src/routes/App.js
@@ -58,7 +58,7 @@ export default function App() {
         </div>
         <Router>
           <NotFound default />
-          <Maps path="/" countriesinfo={data} />
+          <Maps path="PruebaFinal/" countriesinfo={data} />
           <Mundial path="/mundial" />
           <Trend path="/trend" />
         </Router>
